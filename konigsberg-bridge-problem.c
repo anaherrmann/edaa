d1b833d8b96d6c33639a84806cffcdda82306606
Ana <herrmannluiza@gmail.com>
2020-07-07 11:26:51 -0300

    Initial commit

---
+++ konigsberg-bridge-problem.c
@@ -0,0 +1,24 @@
+// represent the konigsberg graphs problem with adjacency list and adjacency matrix
+// undirected and unweighted graph
+
+typedef struct node
+{
+    /* data */
+} node;
+
+typedef struct graph
+{
+    /* data */
+} graph;
+
+graph* create_graph_list(){}
+
+node* insert_edge_list(){}
+
+int** create_graph_matrix(){}
+
+void insert_edge_matrix(){}
+
+void generate_konisberg_briges(){}
+
+int main(){}
